e1f14368f317782adce834b9a4115060a6a46613
kyutaelee <rbxofjqn@gmail.com>
2018-03-05 20:06:59 +0900

    changed sqllite to mysql

--- piglatin/settings.py
+++ piglatin/settings.py
@@ -74,13 +74,18 @@ WSGI_APPLICATION = 'piglatin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'first_mysql',
+        'USER': 'root',
+        'PASSWORD': 'dldl29xo',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
